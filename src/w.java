aa373ac0cd4af05703a6f1f9a4597c13df4c4c87
BT_2N8_30 <sample@bths.edu>
2024-09-16 09:27:05 -0400

    started

--- src/w.java
+++ src/w.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class w{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int userinput;
+        int base1;
+        int base2;
+        System.out.print("What is the base of your number: ");
+        base1 = scan.nextInt();
+        scan.next();
+        System.out.print("What is your number: ");
+        userinput = scan.nextInt();
+        scan.nextLine();
+        System.out.print("What is your number: ");
+        userinput = scan.nextInt();
+        scan.nextLine();
+        System.out.print("What base do you want to convert to: ");
+        base2 = scan.nextInt();
+        scan.nextLine();
+        //convert userinput into a list by using mod 10, then starting from the end of the list using nested for loops convert to base 10, then again using nested for loops for division
+        ArrayList<Integer> number = new ArrayList<>();
+        for ()
 
+    }
 }
